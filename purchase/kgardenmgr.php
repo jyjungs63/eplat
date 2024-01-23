97faa7cf9fe1b40ca3ae6b835885a4a4afb65574
William <82190402+jyjungs63@users.noreply.github.com>
2024-01-23 09:51:29 +0900

    20240123 r2

--- purchase/kgardenmgr.php
+++ purchase/kgardenmgr.php
@@ -70,6 +70,9 @@
                                     &nbsp;&nbsp;
                                     <input class="form-control " id="idNumstudent" type="text" placeholder="원아수">
                                     &nbsp;&nbsp;
+                                    <input class="form-control " id="idStartNum" type="text" placeholder="시작번호"
+                                        value=1></input>
+                                    &nbsp;&nbsp;
                                     <button class="btn btn-outline-primary" type="button" onclick="addChild()">원아아이디생성
                                     </button>
                                 </div>
@@ -321,7 +324,8 @@
         var selectedText = selectedOption.text;
 
         listStudent(selectedText, 2);
-        $("#idClass").val("v0");
+        //$("#idClass").val("v0");
+        $("#idClassname").val(selectedText);
     });
 
     listStudent = (step, sel) => {
@@ -329,6 +333,7 @@
         dispList = (res) => {
             var js = res['json'];
             tab.setData(js);
+            //$("#idClassname").val(res['classnm'])
             //tab.setData(JSON.parse(js));
             CallToast('Student list successfully!!', "success")
         }
@@ -351,22 +356,27 @@
     addChild = () => {
         var selectElement = document.getElementById("idStudent");
         var selectedValue = selectElement.value;
-
         var classname = $("#idClassname").val();
         var nickname = $("#idNick").val();
         var num = $("#idNumstudent").val();
-        var arr = [...Array(Number(num)).keys()];
+        var start = $("#idStartNum").val();
 
-        arr.forEach(el => {
+        if (selectedValue != "" && classname != "" && nickname != "" && num != "") {
 
-            var data = {
-                classnm: classname,
-                id: nickname + el,
-                passwd: nickname + el,
-                step: selectedValue
-            }
-            tab.addRow(data);
-        })
+            var no = 0;
+            var arr = [...Array(Number(num)).keys()];
+            arr.forEach(el => {
+                no = Number(start) + el;
+                var data = {
+                    classnm: classname,
+                    id: nickname + no,
+                    passwd: nickname + no,
+                    step: selectedValue
+                }
+                tab.addRow(data);
+            })
+        } else
+            alert(" 생성 할 학생의 정보를 입력 하세요");
     };
 
     addClassMember = (chos) => { // 학생등록
@@ -483,92 +493,6 @@
         CallAjax("SMethods.php", "POST", options, dispList, dispErr);
     }
 
-    /* Chart.js Charts */
-    // Sales chart
-    var salesChartCanvas = document.getElementById('revenue-chart-canvas').getContext('2d');
-    //$('#revenue-chart').get(0).getContext('2d');
-
-    var salesChartData = {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-                label: 'Digital Goods',
-                backgroundColor: 'rgba(60,141,188,0.9)',
-                borderColor: 'rgba(60,141,188,0.8)',
-                pointRadius: false,
-                pointColor: '#3b8bba',
-                pointStrokeColor: 'rgba(60,141,188,1)',
-                pointHighlightFill: '#fff',
-                pointHighlightStroke: 'rgba(60,141,188,1)',
-                data: [28, 48, 40, 19, 86, 27, 90]
-            },
-            {
-                label: 'Electronics',
-                backgroundColor: 'rgba(210, 214, 222, 1)',
-                borderColor: 'rgba(210, 214, 222, 1)',
-                pointRadius: false,
-                pointColor: 'rgba(210, 214, 222, 1)',
-                pointStrokeColor: '#c1c7d1',
-                pointHighlightFill: '#fff',
-                pointHighlightStroke: 'rgba(220,220,220,1)',
-                data: [65, 59, 80, 81, 56, 55, 40]
-            },
-        ]
-    }
-
-    var salesChartOptions = {
-        maintainAspectRatio: false,
-        responsive: true,
-        legend: {
-            display: false
-        },
-        scales: {
-            xAxes: [{
-                gridLines: {
-                    display: false,
-                }
-            }],
-            yAxes: [{
-                gridLines: {
-                    display: false,
-                }
-            }]
-        }
-    }
-
-    // This will get the first returned node in the jQuery collection.
-    var salesChart = new Chart(salesChartCanvas, {
-        type: 'line',
-        data: salesChartData,
-        options: salesChartOptions
-    })
-
-    // Donut Chart
-    var pieChartCanvas = $('#sales-chart-canvas').get(0).getContext('2d')
-    var pieData = {
-        labels: [
-            'Instore Sales',
-            'Download Sales',
-            'Mail-Order Sales',
-        ],
-        datasets: [{
-            data: [30, 12, 20],
-            backgroundColor: ['#f56954', '#00a65a', '#f39c12'],
-        }]
-    }
-    var pieOptions = {
-        legend: {
-            display: false
-        },
-        maintainAspectRatio: false,
-        responsive: true,
-    }
-    //Create pie or douhnut chart
-    // You can switch between pie and douhnut using the method below.
-    var pieChart = new Chart(pieChartCanvas, {
-        type: 'doughnut',
-        data: pieData,
-        options: pieOptions
-    });
 
     // printClassMember = () => {
     //     tab.print();
